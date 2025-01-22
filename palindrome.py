274deb2ed98b0f3f6da914057c82c838d3924f17
sanjanasolanki45 <sanjnasolanki75@gmail.com>
2025-01-22 21:07:46 +0530

    Add palindrome program

---
+++ palindrome.py
@@ -0,0 +1,10 @@
+s=input("Enter the value:")
+if(s==s[:: -1]):
+    print("This is a palindrome string")
+else:
+        print("This is not a palindrome string")
+#try:
+  #  user_input = int(s)
+#except ValueError:
+  #  pass
+#print(f"The value is: {s}")
